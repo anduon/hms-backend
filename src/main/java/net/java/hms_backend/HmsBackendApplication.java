92a5eef9bdfc7fd7dfd3825d5dd8a7b628a1e0c4
anduon <dgngcan@gmail.com>
2025-05-19 11:42:33 +0700

    Fix lỗi kết nối database và thêm file .env

--- src/main/java/net/java/hms_backend/HmsBackendApplication.java
+++ src/main/java/net/java/hms_backend/HmsBackendApplication.java
@@ -2,11 +2,18 @@ package net.java.hms_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class HmsBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(HmsBackendApplication.class, args);
 	}
 
