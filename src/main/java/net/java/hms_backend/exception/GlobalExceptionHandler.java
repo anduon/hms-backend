355f2fd0c61e4020bda37106105e9e480b291858
anduon <dgngcan@gmail.com>
2025-09-07 16:47:06 +0700

    feat: Fix Swagger Forbidden

--- src/main/java/net/java/hms_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/net/java/hms_backend/exception/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package net.java.hms_backend.exception;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
